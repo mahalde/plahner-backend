da93a9e0ece3d6fd3b7cc58401eb6f9952d02369
mahalde <39100405+mahalde@users.noreply.github.com>
2020-09-21 22:13:37 +0200

    Feature/Create Dockerfile (#4)
    * Added Dockerfile
    
    * Changed AppService to display in what env the app is
    
    * Changed failing test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello from the Plahner Backend!';
+    return `Hello from the Plahner Backend in ${process.env.NODE_ENV} mode!`;
   }
 }
