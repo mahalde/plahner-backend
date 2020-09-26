cd9bcf033b8ab41e41cee02a12b967bd855e347c
mahalde <malte.thienel@gmail.com>
2020-09-26 11:57:44 +0200

    Added config to tag branch workflow

--- scripts/tag_branch.sh
+++ scripts/tag_branch.sh
@@ -2,6 +2,10 @@
 
 version=$(cat package.json | grep -oP '(?<=\"version\": \")[^\"]*')
 
+git config user.name "${1}"
+
+git config user.email "${2}"
+
 git tag -a "v$version" -m "Release of version: $version"
 
 git push --tags
