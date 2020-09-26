7c0ced0f2989c2b67a0f89c88a9d0c01984afc09
mahalde <malte.thienel@gmail.com>
2020-09-26 13:00:49 +0200

    Changed script to inline

--- scripts/deploy_to_server.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-su - docker
-
-docker pull ghcr.io/mahalde/plahner-backend:latest
-
-docker stop plahner-backend
-docker rm plahner-backend
-docker run -dp 80:3000 --name plahner-backend ghcr.io/mahalde/plahner-backend:latest
