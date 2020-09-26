cfbf8ed1190bb30f82d3ac799e8819055541fee6
mahalde <malte.thienel@gmail.com>
2020-09-26 12:57:24 +0200

    Changed Registry URL in deployment script

--- scripts/deploy_to_server.sh
+++ scripts/deploy_to_server.sh
@@ -2,8 +2,8 @@
 
 su - docker
 
-docker pull docker.pkg.github.com/mahalde/plahner-backend/plahner:latest
+docker pull ghcr.io/mahalde/plahner-backend:latest
 
 docker stop plahner-backend
 docker rm plahner-backend
-docker run -dp 80:3000 --name plahner-backend plahner-backend
+docker run -dp 80:3000 --name plahner-backend ghcr.io/mahalde/plahner-backend:latest
