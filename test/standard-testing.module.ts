5837b5f529bcdbf46c93bc93e1775c4711ef614c
mahalde <malte.thienel@gmail.com>
2020-11-12 21:00:54 +0100

    Removed dependence on google code

--- test/standard-testing.module.ts
+++ test/standard-testing.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { when } from 'jest-when';
-import { GCLOUD_TOPIC_NAME } from 'src/shared/shared.constants';
 
 const mockConfigServiceFactory = () => {
   const mockGet = jest.fn();
@@ -9,9 +8,6 @@ const mockConfigServiceFactory = () => {
     get: mockGet,
   };
 
-  when(mockGet)
-    .calledWith(GCLOUD_TOPIC_NAME, expect.any(String))
-    .mockReturnValue('testProject');
   when(mockGet)
     .calledWith('NODE_ENV')
     .mockReturnValue('awesome');
