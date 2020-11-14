925c83932ff1240862333a42db82ed1e1fa2e20c
mahalde <malte.thienel@gmail.com>
2020-11-14 12:45:51 +0100

    Added imap service to connect to IMAP mailbox

---
+++ src/imap/services/imap/imap.service.ts
@@ -0,0 +1,62 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Config } from 'imap';
+import { connect, ImapSimple, ImapSimpleOptions } from 'imap-simple';
+import { LoggerService } from 'src/shared/services/logger.service';
+
+@Injectable()
+export class ImapService implements OnModuleInit {
+  private logger = new LoggerService(ImapService.name);
+  private imapClient!: ImapSimple;
+  private retries = 0;
+
+  constructor(
+    private config: Config,
+    private maxRetries: number,
+    private retryTimeout: number,
+  ) {}
+
+  async onModuleInit() {
+    await this.connectToMailbox();
+  }
+
+  async connectToMailbox() {
+    const allConfig: ImapSimpleOptions = {
+      imap: {
+        ...this.config,
+        tlsOptions: {
+          rejectUnauthorized: false,
+        },
+      },
+      onmail: this.onNewMail,
+    };
+
+    try {
+      this.imapClient = await connect(allConfig);
+      this.imapClient.openBox('INBOX');
+      this.logger.info(
+        `Connected to IMAP mailbox on host '${this.config.host}'`,
+      );
+      this.retries = 0;
+      this.imapClient.on('error', this.onError);
+    } catch (e) {
+      this.logger.error('Error while connecting to IMAP mailbox', e);
+      this.tryToReconnect();
+    }
+  }
+
+  tryToReconnect() {
+    if (this.retries < this.maxRetries) {
+      this.retries++;
+      setTimeout(() => this.connectToMailbox(), this.retryTimeout);
+    }
+  }
+
+  onNewMail = (numberOfMails: number) => {
+    this.logger.info(`New Mails: ${numberOfMails}`);
+  };
+
+  onError = (err: Error) => {
+    this.logger.error('Error during connection to IMAP mailbox', err);
+    this.tryToReconnect();
+  };
+}
