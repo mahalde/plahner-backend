69deef48e0dfbf5bf038b1f77963dae29bf284fd
mahalde <malte.thienel@gmail.com>
2020-11-19 13:26:30 +0100

    Refactored ImapServiceProvider

--- src/imap/imap.module.ts
+++ src/imap/imap.module.ts
@@ -1,39 +1,41 @@
-import { Module, Provider } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Config } from 'imap';
 import { ImapService } from './services/imap/imap.service';
 
-const imapServiceFactory: Provider<ImapService> = {
-  provide: ImapService,
-  useFactory: (configService: ConfigService) => {
-    const keys: Record<
-      string,
-      StringConstructor | NumberConstructor | BooleanConstructor
-    > = {
-      IMAP_USER: String,
-      IMAP_PASSWORD: String,
-      IMAP_HOST: String,
-      IMAP_PORT: Number,
-      IMAP_TLS: Boolean,
-    };
+const imapServiceFactory = (configService: ConfigService) => {
+  const keys: Record<
+    string,
+    StringConstructor | NumberConstructor | BooleanConstructor
+  > = {
+    IMAP_USER: String,
+    IMAP_PASSWORD: String,
+    IMAP_HOST: String,
+    IMAP_PORT: Number,
+    IMAP_TLS: Boolean,
+  };
 
-    const config = Object.entries(keys).reduce(
-      (prev, [key, func]) => ({
-        ...prev,
-        [key.replace('IMAP_', '').toLowerCase()]: func(configService.get(key)),
-      }),
-      {} as Config,
-    );
+  const config = Object.entries(keys).reduce(
+    (prev, [key, func]) => ({
+      ...prev,
+      [key.replace('IMAP_', '').toLowerCase()]: func(configService.get(key)),
+    }),
+    {} as Config,
+  );
 
-    const maxRetries = 5;
-    const retryTimeout = 5000;
+  const maxRetries = 5;
+  const retryTimeout = 5000;
 
-    return new ImapService(config, maxRetries, retryTimeout);
-  },
-  inject: [ConfigService],
+  return new ImapService(config, maxRetries, retryTimeout);
 };
 
 @Module({
-  providers: [imapServiceFactory],
+  providers: [
+    {
+      provide: ImapService,
+      useFactory: imapServiceFactory,
+      inject: [ConfigService],
+    },
+  ],
 })
 export class ImapModule {}
